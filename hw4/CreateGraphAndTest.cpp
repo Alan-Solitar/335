3cb6d791667ccbf49bb227b89abe9d3985964479
Alan-Solitar <alsolita1@gmail.com>
2016-04-26 10:29:06 -0400

    changes

--- hw4/CreateGraphAndTest.cpp
+++ hw4/CreateGraphAndTest.cpp
@@ -29,6 +29,7 @@ void QueryGraph(AdjacencyList &graph, const string &query_filename ) {
 	int origin_label, dest_label;
 	while(reader >> origin_label) {
 		reader >> dest_label;
+		graph.CheckEdge(origin_label,dest_label);
 	}
 	reader.close();
 }
