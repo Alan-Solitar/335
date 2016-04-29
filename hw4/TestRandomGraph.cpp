9d49859e959c314000aa38eae854a017be86af02
Alan-Solitar <alsolita1@gmail.com>
2016-04-28 21:44:46 -0400

    changes

--- hw4/TestRandomGraph.cpp
+++ hw4/TestRandomGraph.cpp
@@ -1,15 +1,19 @@
 #include <cstdlib>
 #include <ctime>
+#include "AdjacencyList.h"
 
 using namespace std;
 
 srand(time(0)); //use current time as seed for random generator
 const int maximum_value = 1000;
-while (true) {
-const int random_variable = rand() % maximum_value + 1;
-cout << random_variable << endl;
-// break from loop based on a condition.
-}
+const int i1 = rand() % maximum_value + 1;
+const int i2 = rand() % maximum_value + 1;
+graph.AddEdge(i1,i2,0);
+graph.AddEdge(i2,i,0);
+
+
+
+
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -17,3 +21,5 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 	const int MAX_NODES = argv[1];
+	AdjacencyList graph;
+	graph.CreateVertices(MAX_NODES);
