32038caee3ea56f768c32556fe98d810e2ae97e3
Alan-Solitar <alsolita1@gmail.com>
2016-04-29 19:43:12 -0400

    changes

--- hw4/UnionFind.cpp
+++ hw4/UnionFind.cpp
@@ -1,5 +1,21 @@
 #include "UnionFind.h"
 
-int UnionFind::find() {
-	
+int UnionFind::find(int x) {
+	if(s[x] <0)
+		return x;
+	else
+		return s[x] = find(s[x]);
+}
+
+void UnionFind::Union(int vert1, int vert2) {
+	if(vertices_[vert1] < vertices_[vert2]) {
+		vertices_[vert1] = vert2;
+	}
+	else {
+		if(vertices_[root1] ==vertices_[root2]) {
+			vertices_[root1]--;	
+		}
+		vertices_[root2] = root1;
+
+	}
 }
