ddcc964f1960214e039ef97b8d27fbe3d893b8c7
Alan-Solitar <alsolita1@gmail.com>
2016-04-29 23:36:27 -0400

    changes

--- hw4/TestRandomGraph.cpp
+++ hw4/TestRandomGraph.cpp
@@ -10,11 +10,12 @@ GenerateAndAdd() {
 	srand(time(0)); //use current time as seed for random generator
 	const int i1 = rand() % maximum_value + 1;
 	const int i2 = rand() % maximum_value + 1;
-	if(!graph.HasEdge(i1,i2)) {
-		graph.AddEdge(i1,i2,0);
-		graph.AddEdge(i2,i,0);
-		disjoint.Union(i1,i2);
-}
+	while(graph.GetNumberOfSets() > 1)
+		if(!graph.HasEdge(i1,i2)) {
+			graph.AddEdge(i1,i2,0);
+			graph.AddEdge(i2,i,0);
+			disjoint.Union(i1,i2);
+	}
 
 PrintStatistics(AdjacencyList &graph) {
 	cout << "Number Of Edges: " <<graph.num_edges <<endl;
