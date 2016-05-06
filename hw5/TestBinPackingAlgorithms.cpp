996fcc193fabdc62e7f7b3f42c2a3c7b54d3f6df
Alan-Solitar <alsolita1@gmail.com>
2016-05-06 10:36:16 -0400

    changes

---
+++ hw5/TestBinPackingAlgorithms.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+srand(time(0)); //use current time as seed for random generator
+for (int i = 0; i < N; ++i) {
+	const 
+	double
+	random_variable = (double) rand() % (double)RAND_MAX;
+	// Push it into the vector of random numbers.
+}
