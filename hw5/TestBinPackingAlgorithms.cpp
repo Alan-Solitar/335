f36236f3a4d77d8ee9c8c536fb60976fb14050e3
Alan-Solitar <alsolita1@gmail.com>
2016-05-11 18:40:30 -0400

    changes

--- hw5/TestBinPackingAlgorithms.cpp
+++ hw5/TestBinPackingAlgorithms.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <iostream>
+
 
 
 //Written by Alan Solitar
@@ -9,15 +11,15 @@
 using namespace std;
 
 vector<double> GenerateRandomNumbers(int n) {
-srand(time(0)); //use current time as seed for random generator
-for (int i = 0; i < N; ++i) {
-	const 
-	double
-	random_variable = (double) rand() % (double)RAND_MAX;
-	// Push it into the vector of random numbers.
+	vector<double> rand_numbers;
+	srand(time(0)); //use current time as seed for random generator
+	for (int i = 0; i < n; ++i) {
+		const double random_variable = rand() / (double)RAND_MAX;
+		// Push it into the vector of random numbers.
+		rand_numbers.push_back(random_variable);
+	}
 }
-
-int number_of_bins_next_fit(vector<double> random_numbers) {
+int NextFitBinPacking(vector<double> random_numbers) {
 	double current_capacity = 0;
 	int number_of_bins = 1;
 	if(random_numbers.size()==0) {
@@ -34,7 +36,7 @@ int number_of_bins_next_fit(vector<double> random_numbers) {
 	}
 	return number_of_bins;
 }
-int number_of_bins_first_fit(vector<double> random_numbers) {
+int FirstFitBinPacking(vector<double> random_numbers) {
 	vector<double> bins;
 	bins.push_back(0);
 	bool placed = false;
@@ -43,7 +45,7 @@ int number_of_bins_first_fit(vector<double> random_numbers) {
 		return 0;
 	}
 	for(auto&num: random_numbers) {
-		for(&bin:bins) {
+		for(auto &bin:bins) {
 			if(bin +num < 1) {
 				bin+=num;
 				placed = true;
@@ -57,18 +59,24 @@ int number_of_bins_first_fit(vector<double> random_numbers) {
 }
 
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		cout << "Usage: " << argv[0] << " <Number of random values>" << endl;
+		return 0;
+	}
+	cout <<"go";
+	const int N = atoi(argv[1]);
+	cout <<"go";
 	// Generate a vector of N random numbers in the range [0.0, 1.0];
 	vector<double> random_numbers = GenerateRandomNumbers(N);
 	const int number_of_bins_next_fit = NextFitBinPacking(random_numbers);
 	const int number_of_bins_first_fit = FirstFitBinPacking(random_numbers);
 	// Sort the random_numbers from larger to smaller
 	std::sort(random_numbers.begin(), random_numbers.end(), std::greater<double>());
-	const int number_of_bins_first_fit_decreasing =   FirstFitBinPacking(random_numbers);
+	const int number_of_bins_first_fit_decreasing = FirstFitBinPacking(random_numbers);
 
-	cout << “Numberofbins:” << endl;
-	cout << “Next Fit: “ << number_of_bins_next_fit<< endl;
-	cout << “First Fit: “ << number_of_bins_first_fit<< endl;
-	cout << “First Fit Decreasing: “ 
-	<< number_of_bins_first_fit_decreasing << endl;
+	cout << "Numberofbins:" << endl;
+	cout << "Next Fit: " << number_of_bins_next_fit<< endl;
+	cout << "First Fit: "<< number_of_bins_first_fit << endl;
+	cout << "First Fit Decreasing: " <<number_of_bins_first_fit_decreasing <<endl; 
 }
